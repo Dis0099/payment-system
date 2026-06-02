a16e3fff9fba1cbd3ebb78f5fdeb3e06e2165a5a
Portfolio Developer <dev@portfolio.local>
2026-06-02 14:50:07 +0300

    Add analytics tracking module (recovered)

--- src/commission.js
+++ src/commission.js
@@ -3,4 +3,14 @@ function calculateCommission(amount, rate) {
     return amount * rate;
 }
 
-module.exports = { calculateCommission };
+function calculateProgressiveCommission(amount, rate) {
+    if (amount < 100) {
+        return amount * rate;
+    } else if (amount < 1000) {
+        return amount * rate * 0.95;
+    } else {
+        return amount * rate * 0.9;
+    }
+}
+
+module.exports = { calculateCommission, calculateProgressiveCommission };
